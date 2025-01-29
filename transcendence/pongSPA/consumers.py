762ace6933e5f2d3147ec782b991f5a36484847f
Melanie Yanez <melanieyanez@gmail.com>
2025-01-29 23:47:43 +0100

    basic AI started

--- transcendence/pongSPA/consumers.py
+++ transcendence/pongSPA/consumers.py
@@ -1,10 +1,27 @@
 import json
+import asyncio
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class PongAIConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+        self.paddle_y = 200  # Position initiale de l'IA
+
+    async def disconnect(self, close_code):
+        pass
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        await self.send(json.dumps({"response": "pong"}))
+        
+        if data["type"] == "ball_position":
+            ball_y = data["y"]
+            
+            #Déplacer le paddle vers la balle
+            self.paddle_y += (ball_y - self.paddle_y) * 0.1
+            
+            #Renvoyer la position mise à jour du paddle
+            await self.send(text_data=json.dumps({
+                "type": "update_paddle",
+                "y": self.paddle_y
+            }))
