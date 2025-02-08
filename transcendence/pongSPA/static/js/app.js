fb1fe231107f2917664f9f23147f5928e7869eee
th1zan <thibault.sanglard@gmail.com>
2025-02-08 21:57:19 +0100

    add bootstrap from Maria

--- transcendence/pongSPA/static/js/app.js
+++ transcendence/pongSPA/static/js/app.js
@@ -199,54 +199,52 @@ function displayRegistrationForm() {
   document.getElementById('app_bottom').innerHTML = '';
 
 
-  const appDiv = document.getElementById("app_main");
-    appDiv.innerHTML = `
-    <div class="d-flex justify-content-center align-items-center" style="min-height: 75vh; background-color: #f8f9fa;">
-      <div class="card p-5 shadow-lg" style="width: 30rem; border-radius: 20px;">
-        <h2 class="text-center mb-5" style="color: #007bff; font-size: 2.5rem;">Create Account</h2>
-        <form id="signupForm">
-          <div class="form-group mb-4">
-            <label for="newUsername" style="font-size: 1.2rem;"><i class="bi bi-person"></i> Username</label>
-            <input 
-              type="text" 
-              id="newUsername" 
-              class="form-control form-control-lg" 
-              placeholder="Enter your username" 
-              required 
-            />
-          </div>
-          <div class="form-group mb-5">
-            <label for="newPassword" style="font-size: 1.2rem;"><i class="bi bi-lock"></i> Mot de passe</label>
-            <input 
-              type="password" 
-              id="newPassword" 
-              class="form-control form-control-lg" 
-              placeholder="Enter your password" 
-              required 
-            />
-          </div>
-          <div class="form-check mb-4">
-            <input type="checkbox" id="privacyPolicyAccepted" required />
-            <label for="privacyPolicyAccepted">
-              I accept the <a href="#" id="privacyPolicyLink">Privacy Policy</a>
-            </label>
-          </div>
-          <button 
-            type="submit" 
-            class="btn btn-success w-100 py-3" 
-            style="font-size: 1.3rem;">
-            Create Account
-          </button>
-        </form>
-        <button 
-          id="backToLoginButton" 
-          class="btn btn-primary w-100 mt-4 py-3" 
-          style="font-size: 1.3rem;">
-          Back to Login
-        </button>
+const appDiv = document.getElementById("app_main");
+appDiv.innerHTML = `
+<div class="d-flex justify-content-center align-items-center" style="min-height: 75vh; background-color: #f8f9fa;">
+  <div class="card p-5 shadow-lg" style="width: 30rem;">
+    <h2 class="text-center mb-5 text-primary" style="font-size: 2.5rem;">Create Account</h2>
+    <form id="signupForm">
+      <div class="form-group mb-4">
+        <label for="newUsername" class="h5"><i class="bi bi-person"></i> Username</label>
+        <input 
+          type="text" 
+          id="newUsername" 
+          class="form-control form-control-lg" 
+          placeholder="Enter your username" 
+          required 
+        />
       </div>
-    </div>
-    `;
+      <div class="form-group mb-5">
+        <label for="newPassword" class="h5"><i class="bi bi-lock"></i> Mot de passe</label>
+        <input 
+          type="password" 
+          id="newPassword" 
+          class="form-control form-control-lg" 
+          placeholder="Enter your password" 
+          required 
+        />
+      </div>
+      <div class="form-check mb-4">
+        <input type="checkbox" id="privacyPolicyAccepted" required />
+        <label for="privacyPolicyAccepted">
+          I accept the <a href="#" id="privacyPolicyLink">Privacy Policy</a>
+        </label>
+      </div>
+      <button 
+        type="submit" 
+        class="btn btn-success w-100 py-3 h5">
+        Create Account
+      </button>
+    </form>
+    <button 
+      id="backToLoginButton" 
+      class="btn btn-primary w-100 mt-4 py-3 h5">
+      Back to Login
+    </button>
+  </div>
+</div>
+`;
 
   document
     .getElementById("signupForm")
@@ -273,7 +271,8 @@ function displayRegistrationForm() {
 export function displayWelcomePage() {
 
   history.pushState({ page: 'welcome' }, 'Welcome', '#welcome');
-   
+  const username = localStorage.getItem("username");
+
   /*when this displayWelcomePage() function is called,
   we can use and fill these HTML containers from index.html,
   they are identified by their id:
@@ -282,53 +281,116 @@ export function displayWelcomePage() {
   - "footer" on the bottom
   - "header" on the top
   */
-  const username = localStorage.getItem("username");
 
-    //empty all the containers
+  //class and CSS definition for the div and navbar (menu) container
+ const appDiv = document.getElementById('app');
+  appDiv.className = 'p-1 h-100 d-flex nav flex-column nav-pills';
+  Object.assign(appDiv, {
+    style: {
+      // width: '200px'
+        backgroundColor: '#343a40'
+    },
+    // role: 'tablist',
+    // 'aria-orientation': 'vertical',
+    // id: 'v-pills-tab'
+  });
+  // Set the background image for 'app'
+  appDiv.style.backgroundImage = "url('/static/pong.jpg')";
+  appDiv.style.backgroundRepeat = "no-repeat";
+  appDiv.style.backgroundAttachment = "fixed";
+  appDiv.style.backgroundSize = "100% 100%";
+
+  //empty all the containers
   document.getElementById('app_top').innerHTML = '';
   document.getElementById('app_main').innerHTML = '';
   document.getElementById('app_bottom').innerHTML = '';
 
+  const appTop = document.getElementById('app_top');
+  appTop.className = "p-1 d-flex rounded";
+  appTop.style.backgroundColor = 'rgba(0, 123, 255, 0.5)'; // Bleu semi-transparent (anciennement bg-primary)
+  appTop.innerHTML = `
+    <div class="d-flex justify-content-between align-items-center w-100">
+      <div>
+        <h2>Bonjour ${username}</h2>
+      </div>
+      <div class="align-self-end">
+        <div class="rounded-circle d-flex align-self-center m-3 overflow-hidden" style="width:100px; height:60%; background-color: red;">
+          <img src="/static/mvillarr.jpg" class="object-fit-cover" alt="mvillarr" width="100%" height="100%" />
+        </div>
+      </div>
+    </div>
+  `;
 
-  // Vérifiez si les conteneurs existent déjà
   const appMain = document.getElementById("app_main");
-
+  appMain.className = "p-3 flex-grow-1";
+  appMain.style.backgroundColor = 'rgba(40, 167, 69, 0.5)'; // Vert semi-transparent (anciennement bg-success)
   appMain.innerHTML = `
-      <h2>Bonjour ${username}</h2>
-    <br>
+    Contenu de la Welcome page
+  `;
+
+  const appBottom = document.getElementById("app_bottom");
+  appBottom.className = "p-3";
+  appBottom.style.backgroundColor = 'rgba(255, 193, 7, 0.5)'; // Jaune semi-transparent (anciennement bg-warning)
+  appBottom.innerHTML = `
+    Footer de la page
   `;
 
   const menuDiv = document.getElementById("menu");
+
   menuDiv.innerHTML = `
-    <button id="playButton">Play a Game</button>
-    <br>
-    <br>
-    <button id="tournamentButton">Tournament</button>
-    <br>
-    <br>
-    <button id="statsButton">Statistics</button>
-    <br>
-    <br>
-    <button id="friendsButton">Friends</button>
-    <br>
-    <br>
-    <button id="settingsButton">Settings</button>
-    <br>
-    <br>
-    <br>
-    <br>
-    <button id="logoutButton">Logout</button>
-      `;
+    <div class="d-flex flex-column h-100" style="background-color: #c2d4de;">
+      <img src="/static/mvillarr.jpg" class="rounded-circle object-fit-cover align-self-center my-4" alt="Mvillarr" width="90" height="100" />
+
+
+      <!-- <button id="playButton" class="btn btn-link nav-link text-white mb-2" role="button" aria-selected="true">Play a Game</button> -->
+      <!-- <button id="tournamentButton" class="btn btn-link nav-link text-white mb-2">Tournament</button> -->
+      <!-- <button id="statsButton" class="btn btn-link nav-link text-white mb-2">Statistics</button> -->
+      <!-- <button id="friendsButton" class="btn btn-link nav-link text-white mb-2">Friends</button> -->
+      <!-- <button id="settingsButton" class="btn btn-link nav-link text-white mb-2">Settings</button> -->
+      <!-- <div class="flex-grow-1"></div> -->
+      <!-- <button id="logoutButton" class="btn btn-link nav-link text-danger">Logout</button> -->
+     
+      
+      <a class="nav-link active mb-2" id="playButton" data-toggle="pill" role="button" aria-selected="true">Play</a>
+      <a class="nav-link active mb-2" id="tournamentButton" data-toggle="pill" role="tab" aria-selected="false">New Tournament</a>
+      <a class="nav-link active mb-2" id="statsButton" data-toggle="pill" role="tab" aria-selected="false">Statistics</a>
+      <a class="nav-link active mb-2" id="friendsButton" data-toggle="pill" role="tab" aria-selected="false">Friends</a>
+      <a class="nav-link active mb-2" id="settingsButton" data-toggle="pill" role="tab" aria-selected="false">Settings</a>
+
+      <a class="nav-link active mb-2" id="welcomeButton" data-toggle="pill" role="tab" aria-selected="false">Return to welcome page</a>
+      <a class="nav-link text-danger" id="logoutButton" data-toggle="pill" role="tab" aria-selected="false">Log out</a>
+       
+    </div>
+  `;
+
+  
+  //section non intégré dans le code pour le moment
+  //
+  // const interactivediv = document.getelementbyid("interactivepart");
+  // interactivediv.innerhtml = `
+  //   <div style="background-image: url(/static/pong.jpg); background-repeat: no-repeat; background-attachment: fixeqd; background-size: 100% 100%;" class="p-1 h-50 d-flex rounded " >
+  //     <div style="background-color: rgba(255, 255, 255, 0.4);" class="p-1 h-50 w-100 d-flex rounded align-self-end justify-content-between" >
+  //       <div class="rounded-circle d-flex align-self-center m-3 overflow-hidden" style="width:100px ; height:60%; background-color: red;">
+  //         <img src="/static/ilyanar.jpg" class="object-fit-cover"  alt="ilkay" width="100%" height="100%" />
+  //       </div>
+  //
+  //       <div class="container row" style="" >
+  //         <div class="col-4"> blabla 1 </div>
+  //         <div class="col-4"> blabla 2 </div>
+  //         <div class="col-4"> blabla 3 </div>
+  //       </div>
+  //
+  //     </div>
+  //   </div>
+  //  `;
 
   document.getElementById("playButton").addEventListener("click", displayGameForm);
   document.getElementById("tournamentButton").addEventListener("click", displayTournament);
   document.getElementById("statsButton").addEventListener("click", displayStats);
   document.getElementById("friendsButton").addEventListener("click", displayFriends);
   document.getElementById("settingsButton").addEventListener("click", displaySettings);
+  document.getElementById("welcomeButton").addEventListener("click", displayWelcomePage);
   document.getElementById("logoutButton").addEventListener("click", logout);
-
-  
-
 }
 
 export function displayTournament() {
@@ -386,7 +448,7 @@ export function displayFriends() {
   document.getElementById('app_main').innerHTML = '';
   document.getElementById('app_bottom').innerHTML = '';
 
-  const appTop = document.getElementById("app_top");
+  const appTop = document.getElementById("app_main");
   appTop.innerHTML = `
     <h3>👥 Friends Management</h3>
     <br>
@@ -419,7 +481,7 @@ function displayHTMLforSettings(user) {
   document.getElementById('app_bottom').innerHTML = '';
   
   const avatarUrl = user.avatar_url ? user.avatar_url : "/media/avatars/default.png";
-  const appTop = document.getElementById("app_top");
+  const appTop = document.getElementById("app_main");
   
   appTop.innerHTML = `
   <div class="container mt-4">
@@ -461,9 +523,9 @@ function displayHTMLforSettings(user) {
 
     <!-- Account Actions -->
     <div class="d-flex justify-content-center mt-4">
-    <button id="deleteAccountButton" class="btn btn-danger px-4" style="margin-right: 38px;">Supprimer le compte</button>
-    <button id="anonymizeAccountButton" class="btn btn-warning">Anonymiser le compte</button>
-      </div>
+      <button id="deleteAccountButton" class="btn btn-link nav-link text-danger">Delete account</button>
+      <button id="anonymizeAccountButton" class="btn btn-link nav-link text-warning">Anonimize account</button>
+    </div>
   `;
 
   document.getElementById("deleteAccountButton").addEventListener("click", deleteAccount);
@@ -744,8 +806,8 @@ export function displayGameForm() {
   const username = localStorage.getItem("username");
   localStorage.setItem("context", "solo");
   
-  const appTop = document.getElementById("app_top");
-  appTop.innerHTML = `
+  const appMain = document.getElementById("app_main");
+  appMain.innerHTML = `
     <h3>Pong Game</h3>
     <form id="gameForm">
       <label for="player1">Player 1 Name:</label>
