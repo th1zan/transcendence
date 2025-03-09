4ba4b7730c029724c791b18481a97cd53e278cc0
th1zan <thibault.sanglard@gmail.com>
2025-03-09 22:52:52 +0100

    NOT FIXED: friend avatar dont display with docker compose

--- transcendence/config/settings_prod.py
+++ transcendence/config/settings_prod.py
@@ -194,7 +194,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = "/media/"
+# MEDIA_URL = "/media/"
+MEDIA_URL = "https://localhost:8443/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 SIMPLE_JWT = {
